750fe5e45c1c402de8705036125156c82d3c12e6
gianmaiolo <127890901+gianmaiolo@users.noreply.github.com>
2023-04-06 11:26:01 +0200

    Add files via upload

---
+++ programma.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import time
+startTime0 = time.time()
+
+import femm 
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+from matplotlib import rc
+import pandas as pd
+
+mpl.rcParams["text.usetex"] = True
+
